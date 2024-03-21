54cdf1376f7cefc20869d041bf9d47163f625492
matbac85 <mathildebaclin@gmail.com>
2024-03-21 13:20:28 +0100

    j included

---
+++ main.js
@@ -0,0 +1,38 @@
+const errorMessage = document.createElement("p");
+const form = document.querySelector(".e-header__form");
+const inputBlock = document.querySelector(".e-header__input-block");
+
+inputBlock.appendChild(errorMessage);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const emailInput = document.querySelector("input[type='email']");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const emailValue = emailInput.value.trim();
+
+    if (emailValue === "") {
+      errorMessage.textContent =
+        "Whoops! It looks like you forgot to add your email";
+    } else if (!isValidEmail(emailValue)) {
+      errorMessage.textContent = "Please provide a valid email address";
+    } else {
+      errorMessage.textContent = "";
+      form.submit();
+      return;
+    }
+
+    errorMessage.classList.add("e-header__error");
+  });
+
+  emailInput.addEventListener("focus", function () {
+    errorMessage.textContent = "";
+    emailInput.value = "";
+  });
+
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+});
